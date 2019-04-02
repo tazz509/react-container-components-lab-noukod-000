065cd66f00c9e7bd50ac77cf8bab75a0719eb94b
carly <carly@noukod.com>
2019-04-02 15:57:28 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const Review = ({ title, summary }) => (
+  <div className="review">
+    <h3>{ title }</h3>
+    <h4>{ summary }</h4>
+  </div>
+)
+
+
+const MovieReviews = ({reviews}) => (
+  <div className="review-list">
+    { reviews.map(review =>
+      <div className="review">
+      <h3>{ review.display_title }</h3>
+      <h4>{ review.summary_short }</h4>
+    </div>) }
+  </div>
+)
+  
+
+
+export default MovieReviews
